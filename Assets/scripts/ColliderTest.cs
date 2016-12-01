4cfd0d87ba659c0d700730083f7938363b3cff96
thijsguelen22 <thijsguelen22@gmail.com>
2016-12-01 09:35:16 +0100

    First commit

---
+++ Assets/scripts/ColliderTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColliderTest : MonoBehaviour {
+
+    // Use this for initialization
+    void start()
+    {
+        Debug.Log("init");
+    }
+    void OnTriggerEnter(Collision col)
+    {
+        if (col.gameObject.name == "finish")
+        {
+            Debug.Log("booped da finish");
+        }
+        if(col.gameObject.name == "checkpoint1")
+        {
+            Debug.Log("booped da shagpoint");
+        }
+    }
+}
